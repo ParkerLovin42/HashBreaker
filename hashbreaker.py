27698f141728cb31099a087e05c3d706b8b0ba11
Parker Lovin <pblovin42@tntech.edu>
2024-11-01 11:36:20 -0400

    Added comments

--- hashbreaker.py
+++ hashbreaker.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
+
 import hashlib
 
-# NOTE TO SELF: Remember to add handling for multiple hashes.
-#		Also, add improved handling for closing files during errors.
-
+# This function prints ethical use information and walks the user through the input process.
+# The function returns a tuple containing the hash type, the file containing the hashes, and the wordlist file.
 def print_info():
+	print("Please use this program only for ethical purposes.")
+	print("Never attempt to crack someone's hash without that person's permission.")
+	print()
+	print()
 	print("Here are the hash types supported by this program: ")
 	print("\t1) SHA-1")
 	print("\t2) SHA-256")
@@ -17,9 +21,10 @@ def print_info():
 	print()
 	return (hash_type, hash_file, wordlist)
 	
+# This function cracks a hash by comparing the hashes of lines in a wordlist against the given hash string.
 def crack_hash(hash_string, wordlist, hash_type):
 	if hash_type == "1":
-		if len(hash_string) != 40:
+		if len(hash_string) != 40:	# SHA1 hashes should have a length of 40.
 			print(hash_string + " is not a valid SHA1 hash!")
 			return
 		for line in wordlist:
@@ -27,7 +32,7 @@ def crack_hash(hash_string, wordlist, hash_type):
 				print("Solution for " + hash_string + ":\t\t\t" + line.strip())
 				return
 	elif hash_type == "2":
-		if len(hash_string) != 64:
+		if len(hash_string) != 64:	# SHA256 hashes should have a length of 64.
 			print(hash_string + " is not a valid SHA256 hash!")
 			return
 		for line in wordlist:
@@ -35,7 +40,7 @@ def crack_hash(hash_string, wordlist, hash_type):
 				print("Solution for " + hash_string + ":\t\t\t" + line.strip())
 				return
 	elif hash_type == "3":
-		if len(hash_string) != 32:
+		if len(hash_string) != 32:	# MD5 hashes should have a length of 32.
 			print(hash_string + " is not a valid MD5 hash!")
 			return
 		for line in wordlist:
@@ -45,16 +50,22 @@ def crack_hash(hash_string, wordlist, hash_type):
 		
 
 def main():
-	(hash_type, hash_file, wordlist) = print_info()
-	
+	(hash_type, hash_file, wordlist) = print_info()		# Obtain the hash type, hash file path, and the wordlist path from the user.
 	h = 0
 	w = 0
+	if not hash_type in ["1","2","3"]:	# Verify that the hash type is valid.
+		print("Invalid hash type! Please enter a value between 1 and 3 with no spaces.")
+		return 0 
+	
+	# Try to open the hash file. If this fails, exit the program.
 	try:
 		hfile = open(hash_file, "r", errors='replace')
 		h = hfile.readlines()
 	except:
 		print("Unable to open hash file.")
 		return 0
+		
+	# Try to open the wordlist file. If this fails, close the hash file and exit the program.
 	try:
 		wfile = open(wordlist, "r", errors='replace')
 		w = wfile.readlines()
@@ -63,10 +74,14 @@ def main():
 		hfile.close()
 		return 0
 	
+	# For each line in the hash file, attempt to crack it.
 	for line in h:
 		if line.strip() and not line.strip().isspace():	# Ignore empty strings and strings that only contain whitespace.
 			crack_hash(line.strip(), w, hash_type)
-
+	
+	# Close files and exit the program.
 	hfile.close()
 	wfile.close()
+	return 1
+	
 main()
